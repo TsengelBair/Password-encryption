f15e2d5cda1871c8bbb750acb6ca137226495237
Цэнгэл Баир <tsengel01@bk.ru>
2024-11-13 12:57:42 +0300

    забыл сохранить файлы Qt

--- passwordencryptor.cpp
+++ passwordencryptor.cpp
@@ -1,6 +1,79 @@
+#include <algorithm>
+#include <QDebug>
+
 #include "passwordencryptor.h"
 
-PasswordEncryptor::PasswordEncryptor()
-{
+/* Константы для генерации псевдослучайных чисел */
+const int a = 401;
+const int c = 179;
 
+PasswordEncryptor::PasswordEncryptor(QString password) : m_password(password)
+{
+}
+
+QVector<int> PasswordEncryptor::makeAsciiArray()
+{
+    QVector<int>asciiArray(m_password.length());
+    for (int i = 0; i < m_password.length(); ++i){
+        asciiArray[i] = m_password[i].unicode();
+    }
+    return asciiArray;
+}
+
+int PasswordEncryptor::calcSeed(const QVector<int> &asciiArray)
+{
+    int seed = 0;
+    for (int val : asciiArray) {
+        seed += val;
+    }
+    return seed;
+}
+
+void PasswordEncryptor::setOriginalSeed(int seed)
+{
+    m_originalSeed = seed;
+}
+
+int PasswordEncryptor::getRand(int &seed, int size)
+{
+    seed = (seed * a + c) % size;
+    return seed;
+}
+
+void PasswordEncryptor::code()
+{
+    m_cryptedArray = makeAsciiArray();
+
+    int seed = calcSeed(m_cryptedArray);
+    setOriginalSeed(seed);
+
+    int indexToSwap;
+    for (int i = 0; i < m_cryptedArray.size(); ++i){
+        indexToSwap = getRand(seed, m_cryptedArray.size());
+        std::swap(m_cryptedArray[i], m_cryptedArray[indexToSwap]);
+    }
+}
+
+void PasswordEncryptor::decode()
+{
+    // Восстанавливаем начальный seed
+    int seed = m_originalSeed;
+
+    // Воссоздаем индексы для обратного восстановления порядка
+    QVector<int>swapIndices(m_cryptedArray.size());
+    for (int i = 0; i < m_cryptedArray.size(); ++i){
+        swapIndices[i] = getRand(seed, m_cryptedArray.size());
+    }
+
+    // Используем воссозданные индексы перестановок в обратном порядке
+    for (int i = m_cryptedArray.size() - 1; i >= 0; --i) {
+        std::swap(m_cryptedArray[i], m_cryptedArray[swapIndices[i]]);
+    }
+
+    // Для теста
+    QString decodedPassword;
+    for (int val : m_cryptedArray){
+        decodedPassword += QChar(val);
+    }
+    qDebug() << decodedPassword;
 }
--- passwordencryptor.h
+++ passwordencryptor.h
@@ -1,11 +1,43 @@
 #ifndef PASSWORDENCRYPTOR_H
 #define PASSWORDENCRYPTOR_H
 
+#include <QVector>
+#include <QString>
 
 class PasswordEncryptor
 {
 public:
-    PasswordEncryptor();
+    /*
+        param - пароль, который нужно зашифровать
+        сохраняем переданный пароль как m_password в конструкторе
+        расшифровать зашифрованный пароль, можно только зная исходный пароль
+    */
+    PasswordEncryptor(QString password);
+    ~PasswordEncryptor() = default;
+    /*
+        В методе code можем обращаться только к паролю, установленному в m_password
+    */
+    void code();
+    void decode();
+    /*
+        Для последующего декода необходимо знать изначальный seed
+        поэтому сохраянем в m_originalSeed
+    */
+    void setOriginalSeed(int seed);
+
+private:
+    // метод, преобращующий пароль (строку) в массив int
+    QVector<int>makeAsciiArray();
+    // метод для вычисления начального seed
+    // param - вектор рассчитанный в предыдущем методе
+    int calcSeed(const QVector<int>&asciiArray);
+    // псевдорандом
+    int getRand(int& seed, int size);
+
+private:
+    QString m_password;
+    int m_originalSeed;
+    QVector<int>m_cryptedArray;
 };
 
 #endif // PASSWORDENCRYPTOR_H
